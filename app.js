8c4304841308183dadc8a8051ba9d956bc3b7534
Azizullah <azizullahcodes@gmail.com>
2025-07-12 20:45:42 -0700

    u

--- app.js
+++ app.js
@@ -53,58 +53,117 @@ _________________________ Answer ____________________________*/
 // let newArray = array.slice(2,4);
 // console.log(newArray)
 /*_______________________ Question no 5 ______________________
-
+: Check Login Access
+Question:
+Create a condition to allow login only if the username is "admin" and the password is "1234".
+Expected Output:
+Allow login only when both are correct.
 _________________________ Answer ____________________________*/
-
-
-
-
-
-/*_______________________ Question no 1 ______________________
-
+// let username = 'admin';
+// let password = '1234';
+// if(username === 'admin' && password === 1234){console.log('you are allowed')}
+// else{console.log('not allowed')}
+/*_______________________ Question no 6 ______________________
+Discount Eligibility
+Question:
+A customer gets a discount if their age is less than 18 OR greater than 60.
+Expected Output:
+Show message: "You are eligible for a discount" only in those cases.
 _________________________ Answer ____________________________*/
+// let age = prompt('enter your age');
+// if(age < 18 || age > 60){console.log('eligible for discount')}
+// else{console.log('not eligible for discount ')}
+/*_______________________ Question no 7 ______________________
+Alarm System
+Question:
+An alarm should ring if door is open OR window is open.
 
-
-
-
-
-/*_______________________ Question no 1 ______________________
-
+Expected Output:
+Show: "Alarm ON" when any of the two is open
 _________________________ Answer ____________________________*/
+// let ques1 = prompt('is door open reply yes/not');
+// let ques2 = prompt('is window open reply yes/no');
+// if(ques1 === 'yes' || ques2 === 'yes'){console.log('alarm on')}
+// else{console.log('alarm off')}
 
-
-
-
-
-/*_______________________ Question no 1 ______________________
-
+/*_______________________ Question no 7 ______________________
+ Number Sign and Type
+Question:
+Check if a number is:
+Positive or Negative
+And then, check if it's even or odd
+Expected Output:
+Examples:
+-5 → "Negative Odd"
+8 → "Positive Even"
 _________________________ Answer ____________________________*/
+// let num = prompt('enter number');
+// if(num >= 1){if(num % 2 === 0){console.log('number is positive and even')}
+// else{console.log('Number is odd')}}
+// else if(num === '0'){console.log('Number is zero')}
+// else{console.log('Number is negative')}
+/*_______________________ Question no 8  ______________________
+Login System
+Question:
+Check:
 
+If username is "admin"
 
+Then check if password is "1234"
 
+If both are correct → "Login Successful"
 
-/*_______________________ Question no 1 ______________________
+If password is wrong → "Incorrect Password"
 
+If username is wrong → "Invalid Username"
 _________________________ Answer ____________________________*/
+// let username = prompt('enter user name');
 
+// if(username === 'admin'){let password = prompt('enter password');
+//     if(password === '1234'){console.log('login successful')}
+// else{console.log('incorrect password')}}
+// else{console.log('invalid user name')}
 
+/*_______________________ Question no 9 ______________________
+Traffic Signal
+Question:
+If signal color is "red", "yellow", or "green":
 
+If "red" → check time > 30 sec → "Too Long!", else "Wait"
 
-/*_______________________ Question no 1 ______________________
+If "yellow" → "Ready"
 
+If "green" → "Go"
 _________________________ Answer ____________________________*/
-
-/*_______________________ Question no 1 ______________________
-
+// let color = prompt('enter color of signal red,yellow or green');
+// if(color === 'red'){let time = prompt('enter time in secods 20,10 etc');
+//     if(time > 30){console.log('Too Long!')}
+//     else{console.log('wait')}
+// }
+// else if(color === 'yellow'){console.log('ready')}
+// else{console.log('go')}
+/*_______________________ Question no 10 ______________________
+nested if statement
 _________________________ Answer ____________________________*/
+// let getAge = document.getElementById('age').value;
 
+// if(getAge >=18){let education = document.getElementById('education').value;
+//     if(education === '12'){document.getElementById('p').innerHTML = 'you are eligible for job'}
+//     else{document.getElementById('p').innerHTML = 'your age is less'}
+// }
+// else{document.getElementById('p').innerHTML = 'You are child'}
 
+// function checkeligibility(){let getAge = document.getElementById('age').value;
+//     if(getAge >= 18){let education = document.getElementById('education').value;
+//         if(education === 'inter'){document.getElementById('p').innerHTML = 'you are eligible'}
+//         else{document.getElementById('p').innerHTML = 'you have low education'}
+//     }
+//     else{document.getElementById('p').innerHTML = 'you have less age than requirement'}
+// }
 
-
-/*_______________________ Question no 1 ______________________
-
+/*_______________________ Question no 11 ______________________
+  
 _________________________ Answer ____________________________*/
-
 
 /*_______________________ Question no 1 ______________________
 
